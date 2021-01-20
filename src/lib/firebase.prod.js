7fc9af97b3ffb511aaa06c869735e275e8f2e63d
mo-adel007 <67648966+mo-adel007@users.noreply.github.com>
2021-01-20 18:26:56 +0200

    Update firebase.prod.js

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -3,12 +3,12 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyCELUBRM_vN30nxmFx5z20AuAekDPdzhjY",
-  authDomain: "netflix-f2e71.firebaseapp.com",
-  projectId: "netflix-f2e71",
-  storageBucket: "netflix-f2e71.appspot.com",
-  messagingSenderId: "20841414009",
-  appId: "1:20841414009:web:b234998bf7deb8445743a8",
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: ,
 };
 
 const firebase = Firebase.initializeApp(config);
